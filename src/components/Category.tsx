98aa5acbff4fbd63c51b63593a7e398f827cbe0b
hadaroadams <hadaroadams1234@gmail.com>
2025-01-07 13:50:36 +0000

    Progress fix

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -28,7 +28,7 @@ const Category = ({ category }: CategoryProps) => {
         <Progress
           value={(category.count / category.totalCount) * 100}
           className={cn("h-2 w-full", progressBg,indicator)}
-          // indicatorClassName={cn("h-2 w-full", )}
+          indicatorClassName={cn("h-2 w-full", )}
         />
       </div>
     </div>
--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -8,17 +8,19 @@ import { cn } from "@/lib/utils";
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ indicatorClassName, className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
-      className
+      "relative h-4 w-full overflow-hidden rounded-full bg-secondary"
     )}
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className={cn("h-full w-full flex-1 bg-primary transition-all")}
+      className={cn(
+        "h-full w-full flex-1 bg-primary transition-all",
+        indicatorClassName
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
